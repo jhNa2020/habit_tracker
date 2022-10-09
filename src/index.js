019e6f6d48babc6702b2660ba542080d6ff4b4a4
junghae <najh66@gmail.com>
2022-10-09 13:03:35 +0900

    index.js modified

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-import SimpleHabit from './components/simpleHabit';
+// import SimpleHabit from './components/simpleHabit';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
   </React.StrictMode>
 );
